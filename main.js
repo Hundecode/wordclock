df826fb1a3c92b0147ba641f8daf21a71ba3270e
Hundecode <Hundecode@users.noreply.github.com>
2022-10-12 03:05:32 +0200

    create mxn board

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-for (let i=0; i< 64; i++){
-    document.getElementById("Board").
-    appendChild(document.createElement("div")).
-    style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? 'green' : 'blue';    
+let rows = 5;
+let columns = 5;
+let border = 1;
+
+//draw 5x5 Board (default)
+document.getElementById('rowsAmount').innerHTML = rows;
+document.getElementById('columnsAmount').innerHTML = columns;
+
+createBoard();
+
+document.getElementById('heightRange').onchange = function() {
+    rows = this.value;
+    document.getElementById('rowsAmount').innerHTML = this.value;
+}
+
+document.getElementById('widthRange').onchange = function() {
+    columns = this.value;
+    document.getElementById('columnsAmount').innerHTML = this.value;
+}
+
+
+function createBoard() {
+    document.getElementById('Board').innerHTML = '';
+    document.getElementById('Board').style.width=(columns*(52+2))+'px';
+    for (let i=0; i<(rows*columns); i++) {
+        let a = document.getElementById('Board').appendChild(document.createElement('div'));
+        a.setAttribute('id', 'field' + i);
+        a.style.backgroundColor = 'blue';
+        a.style.border = border +"px solid #000000";;
+        a.setAttribute('class', 'field');
+    };
 }
